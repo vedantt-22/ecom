2e36e1a2cae400a6479e8617ecdfad6d3aada783
Vedant Karekar <karekarvedant27@gmail.com>
2026-04-07 13:54:02 +0530

    Updated

---
+++ ecommerce-app/frontend/src/features/auth/verify-code/verify-code.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-code',
+  imports: [],
+  templateUrl: './verify-code.component.html',
+  styleUrl: './verify-code.component.css'
+})
+export class VerifyCodeComponent {
+
+}
